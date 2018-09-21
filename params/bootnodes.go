f44bc318728849217e15e43dfad52547c9c708ed
kaziczeriy <kaz@gameternal.com>
2018-09-21 17:18:48 +0800

    add qq node shanghai

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -26,6 +26,7 @@ var BetanetBootnodes = []string{
 // Ropsten test network.
 var AlphanetBootnodes = []string{
 	"snode://7cf1b6b5f1c86baf0efb228ba9864dbe2fa1ae3eafdfbbd635dd318d0eb30e93b2ed294e4b7f7d17740dc9a3faae0116e8d1bc7c7b06d4d1a95a628dd75d406a@192.168.15.166:60609",
+	"snode://a165f05475624a29ce76fd40721fef2408e2de117fb42f0133de861b0072981f8f437cb9b902c3748d860e4214740cda3177f270903f6286ea05745de182410f@118.25.146.113:60609",
 }
 
 var DevBootnodes = []string{}
