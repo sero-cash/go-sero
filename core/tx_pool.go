2f2413685282128a01231cfe26dd435ee2028f2c
kaziczeriy <kaz@gameternal.com>
2018-09-28 15:04:44 +0800

    update log

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -464,14 +464,12 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 		log.Trace("Discarding already known transaction", "hash", hash)
 		return false, fmt.Errorf("known transaction: %x", hash)
 	}
-	start := time.Now()
 	// If the transaction fails basic validation, discard it
 	if err := pool.validateTx(tx, local); err != nil {
 		log.Trace("Discarding invalid transaction", "hash", hash, "err", err)
 		invalidTxCounter.Inc(1)
 		return false, err
 	}
-	log.Error(fmt.Sprintf("add TxPool validateTx done in %v\n", time.Since(start)))
 	// If the transaction pool is full, discard underpriced transactions
 	if uint64(pool.all.Count()) >= pool.config.GlobalSlots+pool.config.GlobalQueue {
 		// If the new transaction is underpriced, don't accept it
