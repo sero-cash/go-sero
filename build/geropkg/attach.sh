190565060e4b06a14e72502edece0d060d2f0e97
bourneac <bourne@gameternal.com>
2018-12-05 23:23:25 +0800

    update build

--- build/geropkg/attach.sh
+++ build/geropkg/attach.sh
@@ -5,5 +5,30 @@ DATADIR="${ROOT}/data"
 if [ ! -z "$1" ]; then
     DATADIR=$1
 fi
+
+function sysname() {
+
+    SYSTEM=`uname -s`
+    if [[ "Darwin" == "$SYSTEM" ]]
+    then
+        echo "Darwin"
+    fi
+
+    if [[ "Linux" == "$SYSTEM" ]]
+    then
+        name=`cat /etc/system-release|awk '{print $1}'`
+        echo "Linux $name"
+    fi
+}
+
+SNAME=`sysname`
+if [[ "Darwin" = "$SNAME" ]];then
+    export DYLD_LIBRARY_PATH=${ROOT}/czero/lib/
+    echo $DYLD_LIBRARY_PATH
+else
+    export LD_LIBRARY_PATH=${ROOT}/czero/lib/
+    echo $LD_LIBRARY_PATH
+fi
+
 ${ROOT}/bin/gero --datadir=${DATADIR} attach
 
