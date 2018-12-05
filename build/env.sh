5ba1609aa30d9681896f4a2e252913452c41b8a2
wincenteam <wincent@gameternal.com>
2018-12-05 20:27:50 +0800

    update env.sh

--- build/env.sh
+++ build/env.sh
@@ -27,59 +27,80 @@ done
 
 mkdir -p "../go-czero-import/czero/lib"
 
-#current system verion is  DARWIN_AMD64
-rm -rf ../go-czero-import/czero/lib/*
-cd "$root/../go-czero-import/czero"
-cp -rf lib_DARWIN_AMD64/* lib/
-DYLD_LIBRARY_PATH="../go-czero-import/czero/lib_DARWIN_AMD64"
-export DYLD_LIBRARY_PATH
+
+function sysname() {
+
+    SYSTEM=`uname -s |cut -f1 -d_`
+
+    if [ "Darwin" == "$SYSTEM" ]
+    then
+        echo "Darwin"
+
+    elif [ "Linux" == "$SYSTEM" ]
+    then
+        name=`uname  -r |cut -f1 -d.`
+        echo Linux-V"$name"
+    else
+        echo "$SYSTEM"
+    fi
+
+
+
+}
+
+SNAME=`sysname`
+
+if [ "Darwin" == "$SNAME" ]
+then
+    rm -rf ../go-czero-import/czero/lib/*
+    cd "$root/../go-czero-import/czero"
+    cp -rf lib_DARWIN_AMD64/* lib/
+    DYLD_LIBRARY_PATH="../go-czero-import/czero/lib_DARWIN_AMD64"
+    export DYLD_LIBRARY_PATH
+elif [ "Linux-V3" == "$SNAME" ]
+then
+   rm -rf ../go-czero-import/czero/lib/*
+   cd "$root/../go-czero-import/czero"
+   cp -rf lib_LINUX_AMD64_V3/* lib/
+   LD_LIBRARY_PATH="../go-czero-import/czero/lib_LINUX_AMD64_V3"
+   export LD_LIBRARY_PATH
+elif [ "Linux-V4" == "$SNAME" ]
+then
+    rm -rf ../go-czero-import/czero/lib/*
+    cd "$root/../go-czero-import/czero"
+    cp -rf lib_LINUX_AMD64_V4/* lib/
+    LD_LIBRARY_PATH="../go-czero-import/czero/lib_LINUX_AMD64_V4"
+    export LD_LIBRARY_PATH
+elif [ "$SNAME" == "Linux-*" ]
+then
+     echo "only support linux kernal v3 or v4"
+     exit
+elif [ "$SNAME" == "MINGW32" ]
+then
+    rm -rf ../go-czero-import/czero/lib/*
+    cd "$root/../go-czero-import/czero"
+    cp -rf lib_WINDOWS_AMD64/* lib/
+else
+   echo "only support Mingw"
+   exit
+fi
+
 cd "$root"
-
-#current system verion is LINUX_AMD64_V3
-#rm -rf ../go-czero-import/czero/lib
-#cd "$root/../go-czero-import/czero"
-#cp -rf lib_LINUX_AMD64_V3/* lib/
-#cd "$root"
-#LD_LIBRARY_PATH="../go-czero-import/czero/lib_LINUX_AMD64_V3"
-#export LD_LIBRARY_PATH
-#cd "$root"
-
-#current system verion is  LINUX_AMD64_V4
-#rm -rf ../go-czero-import/czero/lib
-#cd "$root/../go-czero-import/czero"
-#p -rf lib_LINUX_AMD64_V4/* lib/
-#cd "$root"
-#LD_LIBRARY_PATH="../go-czero-import/czero/lib_LINUX_AMD64_V4"
-#export LD_LIBRARY_PATH
-#cd "$root"
-
-#current system verion is WINDOWS_AMD64
-#rm -rf ../go-czero-import/czero/lib
-#cd "$root/../go-czero-import/czero"
-#cp -rf lib_WINDOWS_AMD64/* lib/
-#cd "$root"
 
 if [ $1 == "linux-v3" ]; then
     cd "$root/../go-czero-import/czero"
     cp -rf lib_LINUX_AMD64_V3/* lib/
     cd "$root"
     unset args[0]
-    LD_LIBRARY_PATH="../go-czero-import/czero/lib_LINUX_AMD64_V3"
-    export LD_LIBRARY_PATH
 elif [ $1 == "linux-v4" ];then
     cd "$root/../go-czero-import/czero"
     cp -rf lib_LINUX_AMD64_V4/* lib/
     cd "$root"
     unset args[0]
-    LD_LIBRARY_PATH="../go-czero-import/czero/lib_DARWIN_AMD64"
-    export LD_LIBRARY_PATH
 elif [ $1 == "darwin-amd64" ];then
-#    cd "$root/../go-czero-import/czero"
-#    ln -s lib_DARWIN_AMD64/* lib/
-#    cd "$root"
-#    unset args[0]
-#    DYLD_LIBRARY_PATH="../go-czero-import/czero/lib_DARWIN_AMD64"
-#    export DYLD_LIBRARY_PATH
+     cd "$root/../go-czero-import/czero"
+     cp -rf lib_DARWIN_AMD64/* lib/
+     cd "$root"
      unset args[0]
 elif [ $1 == "windows-amd64" ];then
     unset args[0]
@@ -87,10 +108,8 @@ elif [ $1 == "windows-amd64" ];then
     cp -rf lib_WINDOWS_AMD64/* lib/
     cd "$root"
 else
-     echo "defalut"
+     echo "local"
 fi
-
-
 
 
 if [ ! -L "$ethdir/go-czero-import" ]; then
