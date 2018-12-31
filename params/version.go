2771a39d26e9f0d3e62dde8fedc23706414cfa59
gordon-sero <gordon@sero.vip>
2018-12-31 21:12:07 +0800

    modify version num

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0                    // Major version component of the current release
 	VersionMinor = 3                    // Minor version component of the current release
 	VersionPatch = 2                    // Patch version component of the current release
-	VersionMeta  = "beta.rc.5.hotfix.2" // Version metadata to append to the version string
+	VersionMeta  = "beta.rc.5-hotfix.2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
