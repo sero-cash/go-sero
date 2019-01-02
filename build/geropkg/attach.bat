2b235476fcf15bec9ab4396fc6fb198b62263143
wincenteam <wincent@gameternal.com>
2019-01-02 15:45:49 +0800

    modify script

--- build/geropkg/attach.bat
+++ build/geropkg/attach.bat
@@ -1,8 +1,6 @@
 @echo off 
 set CURRENT=%cd%
-echo %CURRENT%
 set LIB_PATH=%CURRENT%\czero\lib
-echo %LIB_PATH%
 set path=%LIB_PATH%
-start /b %CURRENT%\bin\gero.exe attach \\.\pipe\gero.ipc
+start /b bin\gero.exe attach \\.\pipe\gero.ipc
 
--- build/geropkg/startup.sh
+++ build/geropkg/startup.sh
@@ -4,31 +4,8 @@ ROOT=$(cd `dirname $0`; pwd)
 
 sh ${ROOT}/stop.sh
 
-function sysname() {
-
-    SYSTEM=`uname -s`
-    if [[ "Darwin" == "$SYSTEM" ]]
-    then
-        echo "Darwin"
-    fi
-
-    if [[ "Linux" == "$SYSTEM" ]]
-    then
-        name=`cat /etc/system-release|awk '{print $1}'`
-        echo "Linux $name"
-    fi
-}
-
-SNAME=`sysname`
-if [[ "Darwin" = "$SNAME" ]];then
-    export DYLD_LIBRARY_PATH=${ROOT}/czero/lib/
-    echo $DYLD_LIBRARY_PATH
-else
-    export LD_LIBRARY_PATH=${ROOT}/czero/lib/
-    echo $LD_LIBRARY_PATH
-fi
-
-
+export DYLD_LIBRARY_PATH=${ROOT}/czero/lib/
+export LD_LIBRARY_PATH=${ROOT}/czero/lib/
 DEFAULT_DATD_DIR="${ROOT}/data"
 LOGDIR="${ROOT}/log"
 DEFAULT_RPCPORT=8545
