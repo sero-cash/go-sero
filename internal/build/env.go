8769954edabbfe333911ca9fb6f20178d27c4fa7
wincenteam <wincent@gameternal.com>
2019-02-21 02:52:29 +0800

    modify env.go

--- internal/build/env.go
+++ internal/build/env.go
@@ -81,7 +81,7 @@ func Env() Environment {
 
 // LocalEnv returns build environment metadata gathered from git.
 func LocalEnv() Environment {
-	env := applyEnvFlags(Environment{Name: "local", Repo: "ethereum/go-sero"})
+	env := applyEnvFlags(Environment{Name: "local", Repo: "sero-cash/go-sero"})
 
 	head := readGitFile("HEAD")
 	if splits := strings.Split(head, " "); len(splits) == 2 {
