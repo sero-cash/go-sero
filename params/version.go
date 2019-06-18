34b9715b67bef93a07c0935e08faea93a1e0455a
gordon-sero <gordon@sero.vip>
2019-06-18 16:54:37 +0800

    fix a bug

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 0           // Major version component of the current release
 	VersionMinor = 7           // Minor version component of the current release
-	VersionPatch = 2           // Patch version component of the current release
-	VersionMeta  = "beta.r7.2" // Version metadata to append to the version string
+	VersionPatch = 3           // Patch version component of the current release
+	VersionMeta  = "beta.r7.3" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
