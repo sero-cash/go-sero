440e6119632177627a0137d9135f7ce7d27fef93
gordon-sero <gordon@sero.vip>
2019-06-25 01:34:25 +0800

    change version

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 0           // Major version component of the current release
 	VersionMinor = 7           // Minor version component of the current release
-	VersionPatch = 4           // Patch version component of the current release
-	VersionMeta  = "beta.r7.4" // Version metadata to append to the version string
+	VersionPatch = 5           // Patch version component of the current release
+	VersionMeta  = "beta.r7.5" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
