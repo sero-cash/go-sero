242003f811f9361a6e8e655f0bb5731a4062fba6
wincenteam <wincent@gameternal.com>
2019-06-29 19:38:58 +0800

    update version

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 0           // Major version component of the current release
 	VersionMinor = 7           // Minor version component of the current release
-	VersionPatch = 6           // Patch version component of the current release
-	VersionMeta  = "beta.r7.6" // Version metadata to append to the version string
+	VersionPatch = 7           // Patch version component of the current release
+	VersionMeta  = "beta.r7.7" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
