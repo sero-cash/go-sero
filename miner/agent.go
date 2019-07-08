8f6949ba6b1070f585799cc57285e6ef7bef243c
bourneac <bourne@gameternal.com>
2019-07-09 01:31:36 +0800

    update a bug

--- miner/agent.go
+++ miner/agent.go
@@ -102,7 +102,7 @@ out:
 
 func (self *CpuAgent) mine(work *Work, stop <-chan struct{}) {
 	if result, err := self.engine.Seal(self.chain, work.Block, stop); result != nil {
-		log.Info("Successfully sealed new block", "number", result.Number(), "hash", result.Hash())
+		log.Info("Successfully sealed new block", "number", result.Number(), "hash", result.Header().HashPos())
 		self.returnCh <- &Result{work, result}
 	} else {
 		if err != nil {
