a634819f916ad31b0fbe40c20d66dccf2e63ffec
gordon-sero <gordon@sero.vip>
2019-07-11 01:52:06 +0800

    refact receipt

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -55,6 +55,10 @@ type Receipt struct {
 	TxHash          common.Hash    `json:"transactionHash" gencodec:"required"`
 	ContractAddress common.Address `json:"contractAddress"`
 	GasUsed         uint64         `json:"gasUsed" gencodec:"required"`
+
+	//Staking
+	PoolIds  []common.Hash
+	ShareIds []common.Hash
 }
 
 type receiptMarshaling struct {
@@ -70,16 +74,6 @@ type receiptRLP struct {
 	CumulativeGasUsed uint64
 	Bloom             Bloom
 	Logs              []*Log
-}
-
-type receiptStorageRLP struct {
-	PostStateOrStatus []byte
-	CumulativeGasUsed uint64
-	Bloom             Bloom
-	TxHash            common.Hash
-	ContractAddress   common.Address
-	Logs              []*LogForStorage
-	GasUsed           uint64
 }
 
 // NewReceipt creates a barebone transaction receipt, copying the init fields.
@@ -147,49 +141,6 @@ func (r *Receipt) Size() common.StorageSize {
 		size += common.StorageSize(len(log.Topics)*common.HashLength + len(log.Data))
 	}
 	return size
-}
-
-// ReceiptForStorage is a wrapper around a Receipt that flattens and parses the
-// entire content of a receipt, as opposed to only the consensus fields originally.
-type ReceiptForStorage Receipt
-
-// EncodeRLP implements rlp.Encoder, and flattens all content fields of a receipt
-// into an RLP stream.
-func (r *ReceiptForStorage) EncodeRLP(w io.Writer) error {
-	enc := &receiptStorageRLP{
-		PostStateOrStatus: (*Receipt)(r).statusEncoding(),
-		CumulativeGasUsed: r.CumulativeGasUsed,
-		Bloom:             r.Bloom,
-		TxHash:            r.TxHash,
-		ContractAddress:   r.ContractAddress,
-		Logs:              make([]*LogForStorage, len(r.Logs)),
-		GasUsed:           r.GasUsed,
-	}
-	for i, log := range r.Logs {
-		enc.Logs[i] = (*LogForStorage)(log)
-	}
-	return rlp.Encode(w, enc)
-}
-
-// DecodeRLP implements rlp.Decoder, and loads both consensus and implementation
-// fields of a receipt from an RLP stream.
-func (r *ReceiptForStorage) DecodeRLP(s *rlp.Stream) error {
-	var dec receiptStorageRLP
-	if err := s.Decode(&dec); err != nil {
-		return err
-	}
-	if err := (*Receipt)(r).setStatus(dec.PostStateOrStatus); err != nil {
-		return err
-	}
-	// Assign the consensus fields
-	r.CumulativeGasUsed, r.Bloom = dec.CumulativeGasUsed, dec.Bloom
-	r.Logs = make([]*Log, len(dec.Logs))
-	for i, log := range dec.Logs {
-		r.Logs[i] = (*Log)(log)
-	}
-	// Assign the implementation fields
-	r.TxHash, r.ContractAddress, r.GasUsed = dec.TxHash, dec.ContractAddress, dec.GasUsed
-	return nil
 }
 
 // Receipts is a wrapper around a Receipt array to implement DerivableList.
---
+++ core/types/receipt_rlp.go
@@ -0,0 +1,93 @@
+package types
+
+import (
+	"io"
+
+	"github.com/sero-cash/go-sero/core/types/vserial"
+
+	"github.com/sero-cash/go-sero/common"
+	"github.com/sero-cash/go-sero/rlp"
+)
+
+type receiptForStorage_Version_0 struct {
+	PostStateOrStatus []byte
+	CumulativeGasUsed uint64
+	Bloom             Bloom
+	TxHash            common.Hash
+	ContractAddress   common.Address
+	Logs              []*LogForStorage
+	GasUsed           uint64
+}
+
+type receiptForStorage_Version_1 struct {
+	PoolIds  []common.Hash
+	ShareIds []common.Hash
+}
+
+// ReceiptForStorage is a wrapper around a Receipt that flattens and parses the
+// entire content of a receipt, as opposed to only the consensus fields originally.
+type ReceiptForStorage Receipt
+
+// EncodeRLP implements rlp.Encoder, and flattens all content fields of a receipt
+// into an RLP stream.
+func (r *ReceiptForStorage) EncodeRLP(w io.Writer) error {
+	vs := vserial.VSerial{}
+	{
+		v0 := &receiptForStorage_Version_0{
+			PostStateOrStatus: (*Receipt)(r).statusEncoding(),
+			CumulativeGasUsed: r.CumulativeGasUsed,
+			Bloom:             r.Bloom,
+			TxHash:            r.TxHash,
+			ContractAddress:   r.ContractAddress,
+			Logs:              make([]*LogForStorage, len(r.Logs)),
+			GasUsed:           r.GasUsed,
+		}
+		for i, log := range r.Logs {
+			v0.Logs[i] = (*LogForStorage)(log)
+		}
+		vs.Versions = append(vs.Versions, &v0)
+	}
+	if len(r.PoolIds) > 0 || len(r.ShareIds) > 0 {
+		v1 := &receiptForStorage_Version_1{}
+		v1.ShareIds = r.ShareIds
+		v1.PoolIds = r.PoolIds
+		vs.Versions = append(vs.Versions, &v1)
+	}
+	return rlp.Encode(w, vs)
+}
+
+// DecodeRLP implements rlp.Decoder, and loads both consensus and implementation
+// fields of a receipt from an RLP stream.
+func (r *ReceiptForStorage) DecodeRLP(s *rlp.Stream) error {
+	var v0 receiptForStorage_Version_0
+	var v1 receiptForStorage_Version_1
+	vs := vserial.VSerial{}
+	vs.Versions = append(vs.Versions, &v0)
+	vs.Versions = append(vs.Versions, &v1)
+	if err := s.Decode(&vs); err != nil {
+		return err
+	}
+
+	if err := (*Receipt)(r).setStatus(v0.PostStateOrStatus); err != nil {
+		return err
+	}
+	{
+		// Assign the consensus fields
+		r.CumulativeGasUsed = v0.CumulativeGasUsed
+		r.Bloom = v0.Bloom
+		r.Logs = make([]*Log, len(v0.Logs))
+		for i, log := range v0.Logs {
+			r.Logs[i] = (*Log)(log)
+		}
+		// Assign the implementation fields
+		r.TxHash = v0.TxHash
+		r.ContractAddress = v0.ContractAddress
+		r.GasUsed = v0.GasUsed
+	}
+	{
+		r.PoolIds = v1.PoolIds
+		r.ShareIds = v1.ShareIds
+	}
+
+	return nil
+}
