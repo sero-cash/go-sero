645191f568c894cee3f8ec28351292e1096b788e
gordon-sero <gordon@sero.vip>
2019-07-19 16:28:34 +0800

    add none to close pool

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -467,6 +467,12 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) (e error) {
 		}
 	}()
 
+	if tx.GetZZSTX().Desc_Cmd.ClosePool != nil {
+		i := 0
+		i++
+		fmt.Println(i)
+	}
+
 	// Heuristic limit, reject transactions over 32KB to prevent DOS attacks
 	if tx.Size() > 3200*1024 {
 		return ErrOversizedData
--- zero/txs/stx/ztx_test.go
+++ zero/txs/stx/ztx_test.go
@@ -48,3 +48,25 @@ func TestRLP(t *testing.T) {
 	fmt.Println(e)
 	fmt.Println(dtx)
 }
+
+func TestClose(t *testing.T) {
+	buf := bytes.Buffer{}
+	w := bufio.NewWriter(&buf)
+
+	tx := T{}
+	tx.Fee.Value = utils.NewU256(2)
+	tx.Fee.Currency = utils.CurrencyToUint256("SERO")
+	tx.Desc_Cmd.ClosePool = &ClosePoolCmd{}
+
+	e := rlp.Encode(w, &tx)
+	fmt.Println(e)
+	w.Flush()
+
+	dtx := T{}
+	stream := rlp.NewStream(&buf, uint64(buf.Len()))
+	_, size, _ := stream.Kind()
+	fmt.Println(size)
+	e = stream.Decode(&dtx)
+	fmt.Println(e)
+	fmt.Println(dtx)
+}
