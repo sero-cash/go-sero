b955313922cafe2255f2176afad58aa2f2d0f55a
wincenteam <wincent@gameternal.com>
2020-10-20 14:55:51 +0800

    update Script

--- build/geropkg/startupEx.sh
+++ build/geropkg/startupEx.sh
@@ -16,7 +16,7 @@ DATADIR_OPTION=${DEFAULT_DATD_DIR}
 
 RPC_OPTION="--rpc --rpcport 8545 --rpcapi exchange,sero,net --rpcaddr 127.0.0.1  --rpccorsdomain=*"
 
-cmd="bin/gero --mineMode --config ${CONFIG_PATH} --datadir ${DATADIR_OPTION} --port ${DEFAULT_PORT}  ${RPC_OPTION} --confirmedBlock 32 --rpcwritetimeout 1800 --exchangeValueSt"
+cmd="bin/gero --mineMode --config ${CONFIG_PATH} --datadir ${DATADIR_OPTION} --port ${DEFAULT_PORT}  ${RPC_OPTION} --confirmedBlock 32 --rpcwritetimeout 1800 --exchangeValueStr"
 mkdir -p $LOGDIR
 
 echo $cmd
